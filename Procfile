731203bccce308920ed1191a1cce56a56cc02a78
Robby Ranshous <rranshous@gmail.com>
2015-02-15 13:05:20 -0500

    Procfile, looping, runnable

---
+++ Procfile
@@ -0,0 +1,2 @@
+image_notifier: bundle exec ruby post_image_notifier.rb $EVENTSTORE_URL
+poller: bundle exec ruby poll_tumblr.rb $EVENTSTORE_URL
--- poll_tumblr.rb
+++ poll_tumblr.rb
@@ -5,44 +5,57 @@
 require 'feedjira'
 require 'eventstore'
 
+$stdout.sync = true
+puts "STARTING POLL TUMBLR"
+
+CONNSTRING = ARGV.shift || 'http://0.0.0.0:2113'
 
 blog_urls = File.readlines('urls.txt').map(&:chomp)
 urls = blog_urls.map{|url| "#{url}/rss"}
-CONNSTRING = 'http://0.0.0.0:2113'
 store = EventStore::Client.new(CONNSTRING)
 feeds = Feedjira::Feed.fetch_and_parse(urls)
 
+SLEEP_TIME = 60 * 10 # 10 mins
 TARGET_STREAM = 'tumblr'
 
-feeds.keys.each do |url|
-  puts "BLOG: #{url}"
-  feed = feeds[url]
-  if feed.is_a?(Fixnum)
-    puts "BAD FEED: #{feed}"
-    next
-  end
-  begin
-    blog_data = {
-      'href' => feed.url,
-      'timesetamp' => nil
-    }
-    if feed.last_modified
-      blog_data.merge!({ 'timestamp' => feed.last_modified.iso8601 })
+begin
+  loop do
+    feeds.keys.each do |url|
+      puts "BLOG: #{url}"
+      feed = feeds[url]
+      if feed.is_a?(Fixnum)
+        puts "BAD FEED: #{feed}"
+        next
+      end
+      begin
+        blog_data = {
+          'href' => feed.url,
+          'timesetamp' => nil
+        }
+        if feed.last_modified
+          blog_data.merge!({ 'timestamp' => feed.last_modified.iso8601 })
+        end
+        store.write_event(TARGET_STREAM, 'observed-blog', blog_data)
+      rescue => ex
+        puts "ERROR: #{ex}"
+      end
+      feed.entries.each do |entry|
+        begin
+          post_data = {
+            'href' => entry.url,
+            'blog' => { 'href' => feed.url },
+            'timestamp' => entry.published.iso8601
+          }
+          store.write_event(TARGET_STREAM, 'observed-post', post_data)
+        rescue => ex
+          puts "ERROR: #{ex}"
+        end
+      end
     end
-    store.write_event(TARGET_STREAM, 'observed-blog', blog_data)
-  rescue => ex
-    puts "ERROR: #{ex}"
+    puts "SLEEPING #{SLEEP_TIME}"
+    sleep SLEEP_TIME
   end
-  feed.entries.each do |entry|
-    begin
-      post_data = {
-        'href' => entry.url,
-        'blog' => { 'href' => feed.url },
-        'timestamp' => entry.published.iso8601
-      }
-      store.write_event(TARGET_STREAM, 'observed-post', post_data)
-    rescue => ex
-      puts "ERROR: #{ex}"
-    end
-  end
+rescue => ex
+  puts "EXCEPTION: #{ex}"
+  raise
 end
