d09380b5f1c478af94fd65aec1cbbca15ac19549
Robby Ranshous <rranshous@gmail.com>
2015-02-20 19:03:45 -0500

    new post notifier

--- Gemfile
+++ Gemfile
@@ -1,5 +1,7 @@
 source 'http://rubygems.org'
 
+gem 'pry', group: :development
 gem 'feedjira', '~> 1.6.0'
 gem 'eventstore', '~> 0.1'
 gem 'httparty'
+gem 'streamworker', '~> 0.0.2', git: 'https://github.com/rranshous/streamworker.git'
---
+++ new_post_notifier.rb
@@ -0,0 +1,17 @@
+require 'streamworker'
+require 'pry'
+
+name 'new-post-notifier'
+handle 'tumblr' => 'observed-post' do |state, event, redis|
+  post_href = event[:body]['href']
+  if redis.sismember('seen', post_href)
+    puts "SEEN, skipping: #{post_href}"
+    next
+  end
+  puts "NEW: #{post_href}"
+  post_data = { href: post_href,
+                blog: event[:body]['blog'],
+                timestamp: event[:body]['timestamp'] }
+  emit 'new-posts', 'tumblr-post', post_data
+  redis.sadd('seen', post_href)
+end
